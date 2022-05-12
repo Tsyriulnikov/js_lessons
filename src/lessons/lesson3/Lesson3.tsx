3d43410ca2de608b80d2d13ca12d86d8af991e67
Tsyriulnikov <max_djvu@mail.ru>
2022-05-12 16:26:22 +0300

    lesson 3 v.1

--- src/lessons/lesson3/Lesson3.tsx
+++ src/lessons/lesson3/Lesson3.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import API from './API';
 import './lesson_3';
+
 
 const Lesson3 = () => {
     const [searchName, setSearchName] = useState('');
@@ -8,14 +9,40 @@ const Lesson3 = () => {
     const [searchNameByType, setSearchNameByType] = useState('');
     const [serachResultByType, setSerachResultByType] = useState('');
 
-    const searchFilm = () => {
-        API.searchFilmsByTitle(searchName)
+
+    const searchFilm = async () => {
+        try {
+            const {data} = await API.searchFilmsByTitle(searchName);
+            const {Search, Error, Response} = data;
+            if (Response === 'True') {
+                setSerachResult(JSON.stringify(Search));
+            } else {
+                setSerachResult(Error);
+            }
+        } catch (e) {
+            console.log(e);
+        }
     };
 
-    const searchByType = (e: React.MouseEvent<HTMLButtonElement>) => {
-        const type: string = e.currentTarget.dataset.t ? e.currentTarget.dataset.t : '';
-        API.searchFilmsByType(searchNameByType, type)
-    }
+        const searchByType = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        try {
+            let triggerType: string = 'movie'
+            if (e.currentTarget.dataset.t) {
+                triggerType = e.currentTarget.dataset.t;
+            }
+
+            const {data} = await API.searchFilmsByType(searchNameByType, triggerType);
+            const {Search, Error, Response} = data;
+            if (Response === 'True') {
+                setSerachResultByType(JSON.stringify(Search));
+            } else {
+                setSerachResultByType(Error);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
 
     return (
         <div>
@@ -31,7 +58,8 @@ const Lesson3 = () => {
 
             <div>
                 <h3><p>Search by type:</p></h3>
-                <input type="text" value={searchNameByType} onChange={(e) => setSearchNameByType(e.currentTarget.value)}/>
+                <input type="text" value={searchNameByType}
+                       onChange={(e) => setSearchNameByType(e.currentTarget.value)}/>
                 <button onClick={searchByType} data-t='movie'>Movie</button>
                 <button onClick={searchByType} data-t='series'>Series</button>
                 <div>
