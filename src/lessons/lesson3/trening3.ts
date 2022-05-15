7481f687fe48e3b9e30b05a012d38c578f938a77
Tsyriulnikov <max_djvu@mail.ru>
2022-05-15 12:14:10 +0300

    lesson3 v.2.3

--- src/lessons/lesson3/trening3.ts
+++ src/lessons/lesson3/trening3.ts
@@ -1,33 +1,35 @@
-export let promise = new Promise((resolve, reject)=> {
+import {rejects} from "assert";
+
+export let promise = new Promise((resolve, reject) => {
         setTimeout(() => resolve("done"), 1000);
     }
 )
 promise.then(
-    result=>alert(result),
-    error=>alert(error)
+    result => alert(result),
+    error => alert(error)
 );
 
-let promise2 = new Promise((resolve,reject)=>{
-    setTimeout(()=>reject(new Error("vse propalo")),1000)
+let promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error("vse propalo")), 1000)
 });
 
 promise2.then(
-    result=>alert(result),
-    error=>alert(error)
+    result => alert(result),
+    error => alert(error)
 )
- const runAwait = async () => {
+const runAwait = async () => {
     let vsePropalo = await promise
     console.log("vsePropalo")
-     }
+}
 
-  runAwait()
+runAwait()
 
 
-let blackHole = new Promise((resolve,reject)=>{
-    for (let i = 0; i <10; i++) {
-         console.log(i)
+let blackHole = new Promise((resolve, reject) => {
+    for (let i = 0; i < 10; i++) {
+        console.log(i)
     }
-resolve("the end")
+    resolve("the end")
 })
 blackHole.then(console.log)
 
@@ -41,15 +43,56 @@ blackHole.then(console.log)
 // delay(3000)
 //     .then(()=>alert("Complited in 3 sec"))
 
-function delay(ms:number) {
+function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 delay(3000)
     .then(() => alert('выполнилось через 3 секунды'));
 
-export function getNumber(){
-    const promise =Promise.resolve(Math.random())
+export function getNumber() {
+    const promise = Promise.resolve(Math.random())
     return promise
 }
-getNumber().then(n=>console.log(n))
+
+getNumber().then(n => console.log(n))
+
+//Example
+
+const repo = {
+
+    save(data: Object) {
+        try {
+            localStorage.setItem("same key", JSON.stringify(data))
+        } catch (error) {
+            return false
+        }
+        return true
+    },
+    saveAsync(data: Object) {
+        const promise: Object = new Promise((resolve, reject) => {
+            try {
+                localStorage.setItem("same key", JSON.stringify(data))
+                resolve()
+            } catch (error) {
+                reject(error)
+            }
+            return promise
+        })
+    }
+}
+
+
+repo.save({mame: "Vasy"})
+
+// const promise = repo.saveAsync({mame: "Vasy"})
+// promise
+//     .then(() => console.log("Saved"))
+//     .catch(error => console.log("Not saved" + error))
+
+const run = async () => {
+    await repo.saveAsync({mame: "Vasy"})
+    console.log("Saved")
+}
+
+run()
